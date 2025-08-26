dd54713cb8cae3cbf9cb5b7faf26e8bfe124b58b
Xaitan <utrustad.sprang_0z@icloud.com>
2025-08-26 11:18:30 +0200

    added caching

--- pokedex/main.go
+++ pokedex/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"time"
 
-	"pokedex/internal/pokeapi" // Use YOUR module name
+	"pokedex/internal/pokeapi"
 )
 
 func main() {
-	pokeClient := pokeapi.NewClient(5 * time.Second)
+	pokeClient := pokeapi.NewClient(5*time.Second, time.Minute*5)
 	cfg := &config{
 		pokeapiClient: pokeClient,
 	}
